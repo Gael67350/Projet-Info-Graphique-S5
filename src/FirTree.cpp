a8a114cfc37db4b35b10132faed44148c3156d39
Gael67350 <gael67350@gmail.com>
2019-02-06 15:45:37 +0100

    Paramétrage des textures
    Signed-off-by: Gael67350 <gael67350@gmail.com>

--- src/FirTree.cpp
+++ src/FirTree.cpp
@@ -172,6 +172,12 @@ void FirTree::loadTextures() {
 	glGenTextures(1, &firTreeTrunkTextureID);
 	glBindTexture(GL_TEXTURE_2D, firTreeTrunkTextureID);
 	{
+		// Textures settings
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, firTreeTrunkRGBImg->w, firTreeTrunkRGBImg->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, (GLvoid*)firTreeTrunkRGBImg->pixels);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
